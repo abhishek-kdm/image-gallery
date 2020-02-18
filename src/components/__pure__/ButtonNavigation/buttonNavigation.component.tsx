89ff1a9306999167bbae299e60d6dfe94431e392
Abhishek S. Kadam <abhishekk19@gmail.com>
2020-02-19 01:41:30 +0530

    button navigation component (styling remaining).

---
+++ src/components/__pure__/ButtonNavigation/buttonNavigation.component.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import './buttonNavigation.style.css';
+
+
+interface ButtonNavigationProps extends React.HTMLAttributes<HTMLDivElement> {
+  navLength: number,
+  start?: number,
+  onPrevious?: (current: number) => void,
+  onNext?: (current: number) => void,
+  children: (current: number) => JSX.Element
+}
+ 
+const ButtonNavigation: React.SFC<ButtonNavigationProps> = ({
+  navLength,
+  start,
+  onPrevious,
+  onNext,
+  children,
+}) => {
+
+  const [current, setCurrent] = useState<number>(start || 0);
+
+  return (
+    <div>
+      <div className='button-container'>
+        {current > 0 && (
+          <button
+            className={'previous primary'}
+            onClick={() => {
+              onPrevious && onPrevious(current);
+              setCurrent((current) => Math.max(current - 1, 0));
+            }}
+          >
+            Previous
+          </button>
+        )}
+        {current <= navLength - 1 && (
+          <button
+            className={'next primary'}
+            onClick={() => {
+              onNext && onNext(current);
+              setCurrent((current) => Math.min(current + 1, navLength - 1))
+            }}
+          >
+            Next
+          </button>
+        )}
+      </div>
+      {children(current)}
+    </div>
+  );
+}
+ 
+export default ButtonNavigation;
