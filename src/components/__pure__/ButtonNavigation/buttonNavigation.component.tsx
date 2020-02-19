a0cc253c89322f0dfd19515645bd148339c2ed1d
Abhishek S. Kadam <abhishekk19@gmail.com>
2020-02-20 00:30:22 +0530

    buttonNavigation component styling and refactor.

--- src/components/__pure__/ButtonNavigation/buttonNavigation.component.tsx
+++ src/components/__pure__/ButtonNavigation/buttonNavigation.component.tsx
@@ -23,28 +23,33 @@ const ButtonNavigation: React.FC<ButtonNavigationProps> = ({
   return (
     <div>
       <div className='button-container'>
-        {current > 0 && (
-          <button
-            className={'previous primary'}
-            onClick={() => {
-              onPrevious && onPrevious(current);
-              setCurrent((current) => Math.max(current - 1, 0));
-            }}
-          >
-            Previous
-          </button>
-        )}
-        {current <= navLength - 1 && (
-          <button
-            className={'next primary'}
-            onClick={() => {
-              onNext && onNext(current);
-              setCurrent((current) => Math.min(current + 1, navLength - 1))
-            }}
-          >
-            Next
-          </button>
-        )}
+        <div>
+          {current > 0 && (
+            <button
+              className={'primary'}
+              onClick={() => {
+                onPrevious && onPrevious(current);
+                setCurrent((current) => Math.max(current - 1, 0));
+              }}
+            >
+              &lArr; Previous
+            </button>
+          )}
+        </div>
+        <small style={{ color: '#c6c6c6', margin: 'auto 3rem' }}>{current + 1} of {navLength}</small>
+        <div>
+          {current <= navLength - 1 && (
+            <button
+              className={'primary'}
+              onClick={() => {
+                onNext && onNext(current);
+                setCurrent((current) => Math.min(current + 1, navLength - 1))
+              }}
+            >
+              Next &rArr;
+            </button>
+          )}
+        </div>
       </div>
       {children(current)}
     </div>
